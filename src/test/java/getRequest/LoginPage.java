ab132c078a1c52f43b50bede673ae7ff23312468
preddem <9885283851mMM@>
2021-02-23 14:28:17 +0530

    Login code

---
+++ src/test/java/getRequest/LoginPage.java
@@ -0,0 +1,31 @@
+package getRequest;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import net.minidev.json.JSONObject;
+import resources.APICallsList;
+import resources.StaticData;
+
+public class LoginPage 
+{
+   @Test
+	public void login_Auth() throws Exception
+	{
+		 	RestAssured.baseURI = APICallsList.LOGIN_URL;
+	        RequestSpecification request = RestAssured.given();
+	 
+	        JSONObject requestParams = new JSONObject();
+	       
+	        requestParams.put(StaticData.USERNAME, "TOOLSQA-Test");
+	        requestParams.put(StaticData.PASSWORD, "Test@@123");
+	 
+	        request.body(requestParams.toJSONString());
+	        Response response = request.post("/Account/v1/User");
+	 
+	        Assert.assertEquals(response.getStatusCode(), 201);
+	        String userID = response.getBody().jsonPath().getString("userID");
+	}
+}
