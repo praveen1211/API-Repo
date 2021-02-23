1a136f7e8af11b8de5b8b138cfb33fc65458d15c
preddem <9885283851mMM@>
2021-02-23 19:37:27 +0530

    code

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -1,14 +1,15 @@
 package runner;
 
-import org.testng.annotations.DataProvider;
 
-import io.cucumber.junit.CucumberOptions;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features = { "./src/test/resources/features/LoginPage/" }, plugin = { "pretty",
 		"json:target/cucumber.json", "html:target/feature-html-reports", "junit:target/cucumber.xml",
-		"rerun:target/rerun.txt" }, glue = "com.tollplus.ntta.steps", monochrome = true, dryRun = false, tags = {
-				"@Test" }, strict = true)
+		"rerun:target/rerun.txt" }, glue = "com.tollplus.ntta.steps", 
+		monochrome = true, dryRun = false, 
+		tags = {"@Test" }, strict = true)
 
-public class TestRunner {
+public class TestRunner extends AbstractTestNGCucumberTests{
 
 }
