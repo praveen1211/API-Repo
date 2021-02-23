d1957b91e868f329283ec5460c3361995df24259
preddem <9885283851mMM@>
2021-02-23 14:58:21 +0530

    code changes...!

---
+++ src/featuresfiles/LoginPage.feature
@@ -0,0 +1,4 @@
+Feature: Login Page features
+  
+  Scenerio: Validating Homepage loaded or not
+  Given Verify the application is loaded or not
---
+++ src/test/java/steps/LoginSteps.java
@@ -0,0 +1,17 @@
+package steps;
+
+import java.security.Provider.Service;
+import com.jayway.restassured.response.Response;
+
+import io.cucumber.java.en.Given;
+
+public class LoginSteps
+{
+	
+	@Given("Verify the application is loaded or not")
+	public void applicationLoaded() throws Exception
+	{
+		
+	}
+}
+
